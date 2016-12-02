b2e29693662df83dadadcf2885915feb6fdc0957
Cody Brunner <rockchalkwushock@icloud.com>
2016-12-02 14:38:29 -0600

    Added config for envvars. Updated .gitignore. Updated server to use envvars.

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { Server } from 'http';
 
+import { PORT } from './config';
+
 const app = express();
 const server = Server(app);
-const port = process.env.PORT || 3000;
 
 app.use(express.static('build'));
 
-server.listen(port, err => {
+server.listen(PORT, err => {
   const message = err ? `Error: ${err}` :
-                        `Server listening on localhost:${port}`;
+                        `Server listening on localhost:${PORT}`;
   return console.log(message);
 });
