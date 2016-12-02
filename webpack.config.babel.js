12995d5d51f12db396d5be1ebbc3a5a71202454e
Cody Brunner <rockchalkwushock@icloud.com>
2016-12-02 10:46:29 -0600

    Added babel presets array to PJ. Added webpack configuration.

---
+++ webpack.config.babel.js
@@ -0,0 +1,29 @@
+import path from 'path';
+import webpack from 'webpack'; // eslint-disable-line
+import packageData from './package.json';
+
+let filename = [packageData.name, packageData.version, 'js']; // eslint-disable-line
+
+export default {
+    entry: path.resolve(__dirname, packageData.main),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: filename.join('.'),
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'stage-0']
+                }
+            }
+        ]
+    },
+    resolve: {
+    extensions: ['', '.js', '.jsx']
+    },
+};
