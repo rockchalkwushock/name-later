7a3c84a76a0774c964a2cb30bdf19ec1596451ee
Cody Brunner <rockchalkwushock@icloud.com>
2016-12-02 13:56:02 -0600

    Changed tests/ --> test/ Changed script path. Updated .eslintrc with globals for it & describe.

---
+++ src/test/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import { App } from '../client/components/App';
+
+describe('<App />', () => {
+  it('should have the name: Layout', () => {
+    const TestComponent = () => <App />;
+    const wrapper = shallow(<TestComponent />);
+    expect(wrapper.name()).to.equal('App');
+  });
+});
