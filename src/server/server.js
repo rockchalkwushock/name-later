8fac2fad448775c0bc573ed045dbf10161cd0f5e
Cody Brunner <rockchalkwushock@icloud.com>
2016-12-02 13:44:17 -0600

    Fixed build rendering issue.

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import { Server } from 'http';
-import path from 'path';
 
 const app = express();
 const server = Server(app);
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static('build'));
 
 server.listen(port, err => {
   const message = err ? `Error: ${err}` :
