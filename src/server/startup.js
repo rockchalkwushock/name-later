7bdc85a2cdefce3d510deb99b9a19733452e7cf2
Cody Brunner <rockchalkwushock@icloud.com>
2016-12-05 14:03:25 -0600

    updated naming.

--- src/server/startup.js
+++ src/server/startup.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-import { app } from './server';
+import { app } from './index';
 import { MONGO_URL, PORT } from './config';
 
 export const startServer = (callback) => {
