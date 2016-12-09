cb4ccc22caf64c6d76c92ebb4c5b5a7b291f0ff3
Cody Brunner <rockchalkwushock@icloud.com>
2016-12-08 20:33:40 -0600

    Added spotifyStrategy.

--- src/server/modules/Auth/passportConfig.js
+++ src/server/modules/Auth/passportConfig.js
@@ -1,5 +1,6 @@
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
+import { Strategy as SpotifyStrategy } from 'passport-spotify';
 
 import User from './model';
 
@@ -20,4 +21,24 @@ const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
       .catch(err => done(err));
 });
 
+// SPOTIFY STRATEGY
+
+const spotifyOptions = {
+  clientID: client_id,
+  clientSecret: client_secret,
+  callbackURL: 'http://localhost:8888/auth/spotify/callback'
+};
+
+const spotifyLogin = new SpotifyStrategy(spotifyOptions,
+                (accessToken, refreshToken, profile, done) => {
+  User.findOrCreate({ spotifyId: profile.id })
+      .then(user => {
+        if (!user) done(null, false);
+        done(null, user);
+      })
+      .catch(err => done(err));
+  User.findOrCreate({ spotifyId: profile.id }, (err, user) => done(err, user));
+});
+
 passport.use(localLogin);
+passport.use(spotifyLogin);
